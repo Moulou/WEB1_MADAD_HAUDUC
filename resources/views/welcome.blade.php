b5895335afa7289e9f65fd4c0efa6bd7636ba0f6
Hamzouz <hamz.m@hotmail.fr>
2016-03-16 15:53:58 +0100

    Ajout Titre accueil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading"></div>
+                <div class="panel-heading">Accueil</div>
                 <h2 class="text-center"> Bienvenue sur la plateforme de l'IIM</h2>
                 <br/>
                     <div class="row">
