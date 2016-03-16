263c2e318b3cf593a9fec66c6dabba44a3cf88ac
Moulou <emilie.hauduc@devinci.fr>
2016-03-16 22:44:14 +0100

    ValidateRequest edit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -111,7 +111,7 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Requests\ValidatePostRequest $request, $id)
     {
         //
         $post = Post::find($id);
