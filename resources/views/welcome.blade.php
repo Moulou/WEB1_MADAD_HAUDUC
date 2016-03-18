4585a11c9fa38592f6c1f7965f9243fe184d0a15
Moulou <emilie.hauduc@devinci.fr>
2016-03-18 13:58:29 +0100

    Gestion bouton page accueil quand pas connecte

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,14 +10,15 @@
                 <br/>
                     <div class="row">
                         <div class="col-md-5 col-md-offset-1">
-
                                 <a href="{{route('posts.index')}}">
                                     <button class="btn btn-lg btn-block btn-success">Voir les articles</button>
                                 </a>
                             <br/>
+                            @if(\Illuminate\Support\Facades\Auth::check())
                             <a href="{{route('posts.create')}}">
                                 <button class="btn btn-lg btn-block btn-info">Ecrire un article</button>
                             </a>
+                            @endif
                             <br/>
 
                         </div>
@@ -26,10 +27,13 @@
                             <a href="{{route('projet.index')}}">
                                 <button class="btn btn-lg btn-block btn-success">Voir les projets</button>
                             </a>
+
                             <br/>
+                            @if(\Illuminate\Support\Facades\Auth::check())
                             <a href="{{route('projet.create')}}">
                                 <button class="btn btn-lg btn-block btn-info">Soumettre un projet</button>
                             </a>
+                            @endif
                         </div>
 
                         @if(Auth::check() && Auth::user()->admin == 1)
