a694062d648268c09138870e707508125840aa5e
Hamzouz <hamz.m@hotmail.fr>
2016-03-20 16:43:26 +0100

    Finition fonctionnalite admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,47 +13,53 @@
 
                         @if($project->etat == 0){{--Lorsque le projet est refusé--}}
                             <p style="color:red;" class="pull-right">Projet refusé <i class="fa fa-times"></i></p>
-                            {!! Form::model($project, ['route' => ['admin.update', $project->id], 'method' => 'PUT']) !!}
-                            {!! Form::hidden('admin', 'admin') !!}
-                            {!! Form::hidden('etat', 1) !!}
-                            {!! Form::submit('Mettre en attente', ['class' => 'btn btn-warning']) !!}
-                            {!! Form::close() !!}
+                        <hr>
+                            <form action="{{route('admin.update', $project->id)}}" method="POST">
+                                {{csrf_field()}}
+                                <input type="hidden" name="_method" value="PUT">
+                                <input type="hidden" name="etat" value="1">
+                                <button class="btn btn-warning pull-left">Mettre en attente <i class="fa fa-clock-o"></i></button>
+                            </form>
+                        <form action="{{route('admin.update', $project->id)}}" method="POST">
+                            {{csrf_field()}}
+                            <input type="hidden" name="_method" value="PUT">
+                            <input type="hidden" name="etat" value="2">
+                            <button class="btn btn-success pull-left">Valider <i class="fa fa-check"></i></button>
+                        </form>
 
-                            {!! Form::model($project, ['route' => ['admin.update', $project->id], 'method' => 'PUT']) !!}
-                            {!! Form::hidden('admin', 'admin') !!}
-                            {!! Form::hidden('etat', 2) !!}
-                            {!! Form::submit('Valider', ['class' => 'btn btn-success']) !!}
-                            {!! Form::close() !!}
 
                         @elseif($project->etat == 1){{--Lorsque le projet est en attente de modération--}}
                             <p style="color:darkgrey;" class="pull-right">Projet en attente <i class="fa fa-clock-o"></i></p>
-                            {!! Form::model($project, ['route' => ['admin.update', $project->id], 'method' => 'PUT']) !!}
-                            {!! Form::hidden('admin', 'admin') !!}
-                            {!! Form::hidden('etat', 0) !!}
-                            {!! Form::submit('Refuser', ['class' => 'btn btn-danger']) !!}
-                            {!! Form::close() !!}
-
-                            {!! Form::model($project, ['route' => ['admin.update', $project->id], 'method' => 'PUT']) !!}
-                            {!! Form::hidden('admin', 'admin') !!}
-                            {!! Form::hidden('etat', 2) !!}
-                            {!! Form::submit('Valider', ['class' => 'btn btn-success']) !!}
-                            {!! Form::close() !!}
+                        <hr>
+                        <form action="{{route('admin.update', $project->id)}}" method="POST">
+                            {{csrf_field()}}
+                            <input type="hidden" name="_method" value="PUT">
+                            <input type="hidden" name="etat" value="0">
+                            <button class="btn btn-danger pull-left">Refuser <i class="fa fa-times"></i></button>
+                        </form>
+                        <form action="{{route('admin.update', $project->id)}}" method="POST">
+                            {{csrf_field()}}
+                            <input type="hidden" name="_method" value="PUT">
+                            <input type="hidden" name="etat" value="2">
+                            <button class="btn btn-success pull-left">Validé <i class="fa fa-check"></i></button>
+                        </form>
 
                         @elseif($project->etat == 2){{--Lorsque le projet est validé--}}
                             <p style="color:green;" class="pull-right">Projet validé <i class="fa fa-check"></i></p>
-                            {!! Form::model($project, ['route' => ['admin.update', $project->id], 'method' => 'PUT']) !!}
-                            {!! Form::hidden('admin', 'admin') !!}
-                            {!! Form::hidden('etat', 0) !!}
-                            {!! Form::submit('Refuser', ['class' => 'btn btn-danger']) !!}
-                            {!! Form::close() !!}
-
-                            {!! Form::model($project, ['route' => ['admin.update', $project->id], 'method' => 'PUT']) !!}
-                            {!! Form::hidden('admin', 'admin') !!}
-                            {!! Form::hidden('etat', 1) !!}
-                            {!! Form::submit('Mettre en attente', ['class' => 'btn btn-warning']) !!}
-                            {!! Form::close() !!}
+                        <hr>
+                        <form action="{{route('admin.update', $project->id)}}" method="POST">
+                            {{csrf_field()}}
+                            <input type="hidden" name="_method" value="PUT">
+                            <input type="hidden" name="etat" value="1">
+                            <button class="btn btn-warning pull-left">Mettre en attente <i class="fa fa-clock-o"></i></button>
+                        </form>
+                        <form action="{{route('admin.update', $project->id)}}" method="POST">
+                            {{csrf_field()}}
+                            <input type="hidden" name="_method" value="PUT">
+                            <input type="hidden" name="etat" value="0">
+                            <button class="btn btn-danger pull-left">Refuser <i class="fa fa-times"></i></button>
+                        </form>
                         @endif
-
                         <a href="{{route('admin.show', $project->id)}}">
                             <button class="btn btn-default">Voir le projet en entier</button>
                         </a>
